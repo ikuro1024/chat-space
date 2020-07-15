e9cd4f87ec09d5cfc0b9bc911f076d45d25e8d68
ikuro1024 <ikuro1326@gmail.com>
2020-07-15 16:32:58 +0900

    updating

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="list">
+        `<div class="list" data-message-id=${message.id}>
           <div class="list-box">
             <div class="list-box__name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="list">
+      `<div class="list" data-message-id=${message.id}>
         <div class="list-box">
           <div class="list-box__name">
             ${message.user_name}
@@ -61,6 +61,7 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.form__btn').prop('disabled', false);
     });
   });
 });
